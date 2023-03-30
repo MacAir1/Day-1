786d56897fbc59685ca5ee660df1fe32f1c92bec
김규혁 <gyuhyeok@MacBookAir.local>
2023-03-30 17:18:43 +0900

    end

--- 0330/my-portfolio/src/App.jsx
+++ 0330/my-portfolio/src/App.jsx
@@ -17,6 +17,75 @@ function App() {
           </ul>
         </div>
       </header>
+      <main>
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          소개 내용
+        </div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-100 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
+        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 연락처
+        </div>
+      </main>
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+        By Kim
+      </footer>
     </div>
   );
 }
